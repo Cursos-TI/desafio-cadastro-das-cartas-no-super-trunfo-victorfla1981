402339cea51ad140140e78df458375fd17de2b3c
victorfla1981 <victorfla19812009@gmail.com>
2025-03-26 13:01:59 +0000

    Realizei o desafio CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+    // Carta 1
+    char Estado1[5] = "A";
+    char Codigo1[5] = "A01";
+    char Cidade1[9] = "Salvador";
+    int Populacao1 = 3000000;
+    float Area1 = 693.442;
+    float PIB1 = 62.954;
+    int NumeroDePontosTuristicos1 = 50;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Informacoes da Carta 1
+    printf("Carta 1:\n");
+    printf("O Estado é: %s\n", Estado1);
+    printf("O Codigo é: %s\n", Codigo1);
+    printf("A Cidade é: %s\n", Cidade1);
+    printf("A Populacao é: %d\n", Populacao1);
+    printf("Area é: %.3f\n", Area1);
+    printf("O PIB é: %.3f bilhões de reais\n", PIB1);
+    printf("O Numero de Pontos Turisticos é: %d\n", NumeroDePontosTuristicos1);
+    printf("\n");
+
+    // Carta 2
+    char Estado2[5] = "B";
+    char Codigo2[5] = "B02";
+    char Cidade2[15] = "Porto Alegre";
+    int Populacao2 = 2000000;
+    float Area2 = 423.442;
+    float PIB2 = 88.954;
+    int NumeroDePontosTuristicos2 = 32;
+
+    // Informacoes da Carta 2
+    printf("Carta 2:\n");
+    printf("O Estado é: %s\n", Estado2);
+    printf("O Codigo é: %s\n", Codigo2);
+    printf("A Cidade é: %s\n", Cidade2);
+    printf("A Populacao é: %d\n", Populacao2);
+    printf("Area é: %.2f KM²\n", Area2);
+    printf("O PIB é: %.2f bilhões de reais\n", PIB2);
+    printf("O Número de Pontos Turísticos é: %d\n", NumeroDePontosTuristicos2);
 
     return 0;
 }
