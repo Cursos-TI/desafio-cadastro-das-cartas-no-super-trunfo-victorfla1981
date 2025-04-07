9549d4eff610dab53e4964297b392b3a631a29cd
victorfla1981 <victorfla19812009@gmail.com>
2025-04-07 12:07:16 -0300

    Update CartasSuperTrunfoNivelAventureiro.c

--- CartasSuperTrunfoNivelAventureiro.c
+++ CartasSuperTrunfoNivelAventureiro.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
 
+int main() {
+    // Carta 1
+    char Estado1[5] = "A";
+    char Codigo1[5] = "A01";
+    char Cidade1[10] = "Salvador";
+    double Populacao1 = 2417.678;
+    float Area1 = 693.442;
+    float PIB1 = 62.954;
+    int Numero_de_Pontos_Turisticos1 = 50;
+    float Densidade_Populacional1 = Populacao1 / Area1;
+    float PIB_Per_Capita1 = PIB1 / Populacao1;
+
+    // Informações da Carta 1
+    printf("Carta 1:\n");
+    printf("O Estado é: %s\n", Estado1);
+    printf("O Código é: %s\n", Codigo1);
+    printf("A Cidade é: %s\n", Cidade1);
+    printf("A População é: %.3f mil pessoas\n", Populacao1);
+    printf("Área é: %.2f mil KM²\n", Area1);
+    printf("O PIB é: %.2f bilhões de reais\n", PIB1);
+    printf("O Número de Pontos Turísticos é: %d\n", Numero_de_Pontos_Turisticos1);
+    printf("A densidade populacional é: %.3f pessoas por KM²\n", Densidade_Populacional1);
+    printf("O PIB per capita é: %.3f mil reais por pessoa\n\n", PIB_Per_Capita1);
+
+    // Carta 2
+    char Estado2[5] = "B";
+    char Codigo2[5] = "B02";
+    char Cidade2[15] = "Porto Alegre";
+    double Populacao2 = 1332.845;
+    float Area2 = 423.442;
+    float PIB2 = 88.954;
+    int Numero_de_Pontos_Turisticos2 = 32;
+    float Densidade_Populacional2 = Populacao2 / Area2;
+    float PIB_Per_Capita2 = PIB2 / Populacao2;
+
+    // Informações da Carta 2
+    printf("Carta 2:\n");
+    printf("O Estado é: %s\n", Estado2);
+    printf("O Código é: %s\n", Codigo2);
+    printf("A Cidade é: %s\n", Cidade2);
+    printf("A População é: %.3f mil pessoas\n", Populacao2);
+    printf("Área é: %.2f mil KM²\n", Area2);
+    printf("O PIB é: %.2f bilhões de reais\n", PIB2);
+    printf("O Número de Pontos Turísticos é: %d\n", Numero_de_Pontos_Turisticos2);
+    printf("A densidade populacional é: %.3f pessoas por KM²\n", Densidade_Populacional2);
+    printf("O PIB per capita é: %.3f mil reais por pessoa\n", PIB_Per_Capita2);
+
+    return 0;
+}
